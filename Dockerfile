66148855dabab0c502b41fc103e4da1aaf356df5
spacedebris <kozlowskimarekamil@gmail.com>
2022-07-21 23:26:52 +0200

    connected ordinary 2 mongo

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@
  COPY package.json .
 
  ARG NODE_ENV
- RUN if [ "$NODE_ENV" = "development"]; \
+ RUN if [ "$NODE_ENV" = "development" ]; \
     then npm install; \
     else npm install --only=production; \
     fi
--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose.connect("mongodb://spacedebris:spacedebris@172.22.0.2:27017/?authSource=admin"
+).then(() => console.log('connected 2 mongo'))
+.catch((e) => console.log(e));
 
 app.get("/", (req, res) => {
     res.send("<h3>Do it good ?!</h3><h4> listening on port " +port);
