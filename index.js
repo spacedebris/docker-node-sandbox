32a9e2817a899e12878732ed2b962e42edbb79d0
spacedebris <kozlowskimarekamil@gmail.com>
2022-07-25 12:23:30 +0200

    connection with mongo

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require("./config/config");
 
 const app = express();
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
 
-mongoose.connect("mongodb://spacedebris:spacedebris@172.22.0.2:27017/?authSource=admin"
-).then(() => console.log('connected 2 mongo'))
-.catch((e) => console.log(e));
+const connectWithRetry = () => {
+    mongoose.connect(mongoURL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        //useFindAndModify: false
+    })
+    .then(() => console.log('connected 2 mongo'))
+    .catch((e) => {
+        console.log(e);
+        setTimeout(connectWithRetry, 5000);
+    });
+}
+
+connectWithRetry();
+
 
 app.get("/", (req, res) => {
     res.send("<h3>Do it good ?!</h3><h4> listening on port " +port);
